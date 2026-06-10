4439f6ee89305e9fa6e2f2b7b2f7ed4ad3e25e9e
amanshakyano-ux <amanshakyno@gmail.com>
2026-06-10 14:27:11 +0530

    checking routes

--- routes/salonRoutes.js
+++ routes/salonRoutes.js
@@ -4,6 +4,12 @@ const { loginUser, signupUser } = require("../controller/auth");
 const { searchSalons, getSalon } = require("../controller/salon");
 const upload = require("../utils/upload");
 const router = express.Router();
+router.get("/ping", (req, res) => {
+  res.json({
+    success: true,
+    message: "Salon route working",
+  });
+});
 
 router.get("/search", searchSalons);
 router.get("/:salonId", getSalon);
